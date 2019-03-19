e2c09b10e87b86de611a809b3e3c23744d06fe84
unknown <schumact@mail.uc.edu>
2019-03-19 12:19:26 -0400

    Added function for solution

--- Section6/multiple_pointers_chall_6.js
+++ Section6/multiple_pointers_chall_6.js
@@ -4,17 +4,13 @@
 
 function averagePair(arr, target_num){
     let point1 = 0;
-    let average = 0;
     let count = 0;
 
 
     for (; point1 <= arr.length -1;point1++) {
         let point2 = point1 + 1;
         for (; point2 <= arr.length -1; point2++){
-            console.log('point 1: ', arr[point1]);
-            console.log('point 2 ', arr[point2]);
             if ((arr[point1] + arr[point2]) / 2 === target_num) {
-                average = (arr[point1] + arr[point2]) / 2;
                 console.log(`Found a match: ${arr[point1]} and ${arr[point2]} left me with ${(arr[point1] + arr[point2]) / 2}`);
                 count ++;
             }
@@ -24,3 +20,18 @@ function averagePair(arr, target_num){
   }
 
   averagePair([1,2,3,4,5], 2);
+
+  // actual answer requiring simply a true or false to see if 
+  // such an average existed with 0(N) time 
+  // arr is a sorted array of ints
+  function averagePair2(arr, target_num) {
+    let start = 0
+    let end = arr.length - 1 ;
+    while (start < end) {
+        let avg = (arr[start] + arr[end]) / 2;
+        if (avg === target_num) return true;
+        else if (avg < target_num) start++;
+        else end--;
+    }
+    return false;
+  }
