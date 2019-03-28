a9e9c992ad4b4d7763a1a7592410f102accc4d0a
unknown <schumact@mail.uc.edu>
2019-03-28 19:50:38 -0400

    solutions to a couple more problems

--- Section7/harderRecursionChallenges.js
+++ Section7/harderRecursionChallenges.js
@@ -12,9 +12,36 @@ function reverse(str, new_str) {
 // the func is a palindrome else return false
 function isPalindrome(str) {
     if (str.length === 0) return true;
-    else if (str[0] === str[str.length -1]) {
-        return isPalindrome(str.slice(1,-1))
-    } else{
-        return false;
-    }
+    if (str[0] === str[str.length -1]) return isPalindrome(str.slice(1,-1)); 
+    return false;
 }
+
+// recursivefunc that accepts an array and a callback
+// returns true if a single value in the array 
+// returns true when passed to the callback. Otherise
+// it returns false
+function someRecursive(arr, callback) {
+    if (arr.length === 0) return false;
+    if (callback(arr[0])) return true;
+    return someRecursive(arr.slice(1), callback);
+}
+
+// recursive func that accepts an array of arrays
+// and returns a new array with all values flattened
+function flatten(arr){
+    let new_arr = []
+        for(let i = 0; i < arr.length; i++){
+          if(Array.isArray(arr[i])){
+                new_arr = new_arr.concat(flatten(arr[i]))
+          } else {
+                new_arr.push(arr[i])
+          }
+    } 
+    return new_arr;
+  }
+
+// cleaner solution for reverse()
+// function reverse(str){
+// 	if(str.length <= 1) return str;
+// 	return reverse(str.slice(1)) + str[0];
+// }
