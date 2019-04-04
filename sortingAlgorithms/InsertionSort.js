61cbda3cf45e802dd955eea219eb868b31e64297
unknown <schumact@mail.uc.edu>
2019-04-04 15:36:19 -0400

    Insertion sort solution

---
+++ sortingAlgorithms/InsertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+    for (let i = 1; i <= arr.length - 1; i++) {
+        for (let j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]) swap(arr, j, (j -1));
+        }
+    }
+    return arr;
+}
+
+function swap(arr, indx1, indx2) {
+    let temp = arr[indx1];
+    arr[indx1] = arr[indx2];
+    arr[indx2] = temp;
+}
