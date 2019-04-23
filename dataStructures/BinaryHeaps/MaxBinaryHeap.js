d81ae31dca425398de4efd75e7dd14d2938f74c3
unknown <schumact@mail.uc.edu>
2019-04-23 16:51:47 -0400

    Added extractMax method for MaxBinaryHeap

--- dataStructures/BinaryHeaps/MaxBinaryHeap.js
+++ dataStructures/BinaryHeaps/MaxBinaryHeap.js
@@ -25,13 +25,55 @@ class MaxBinaryHeap {
         this.values = arr;
         return this.values;
     }
+
+    extractMax() {
+        let parent = this.values[0]; 
+        this.values[0] = this.values[this.values.length - 1];
+        this.values.pop();
+        let parent_index = 0;
+        function sink_down(parent_indx, arr) {
+            let parent = arr[parent_indx];
+            let left_index = 2 * parent_indx + 1;
+            let right_index = 2 * parent_indx + 2;
+            let left_child = arr[left_index];
+            let right_child = arr[right_index];
+            let larger_index;
+            if (left_child == null) {
+                return arr;
+            } else if (right_child == null) {
+                larger_index = left_index;
+            } else if (left_child > right_child) {
+                larger_index = left_index;
+            } else {
+                larger_index = right_index;
+            }
+            if (arr[larger_index] > parent) {
+                let temp = arr[larger_index];
+                arr[larger_index] = parent;
+                arr[parent_indx] = temp;
+                return sink_down(larger_index, arr);
+            }
+            return arr;
+        }
+        let arr = sink_down(parent_index, this.values);
+        this.values = arr;
+        return parent;
+    }
 }
 
-t = new MaxBinaryHeap();
-t.insert(10);
-t.insert(15);
-t.insert(4);
-t.insert(18);
-t.insert(6);
-t.insert(9);
-console.log(t);
+heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+console.log(heap);
